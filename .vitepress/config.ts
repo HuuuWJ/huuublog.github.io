8d58c4c373faa5865577d797923a2068612f9d4d
HuuuWJ <hwjieee521@163.com>
2023-04-21 17:48:59 +0800

    test

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -91,7 +91,6 @@ const customElements = [
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-    base: 'https://huuublog.github.io/',
     title: '个人',
     markdown: {
         config: (md) => {
